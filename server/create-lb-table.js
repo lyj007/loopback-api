4f9060c08c5b8f25a55987b53e89efd855c32ab9
lyj.liao <lyj.liao@coloseo.cn>
2019-01-15 18:00:34 +0800

    update

--- server/create-lb-table.js
+++ server/create-lb-table.js
@@ -2,7 +2,7 @@
 /* eslint max-len: 0 */
 const server = require('./server');
 const ds = server.dataSources.db;
-const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Person'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
